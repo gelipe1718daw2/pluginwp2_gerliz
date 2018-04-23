755139cad646ad36215bfc1954429a4224fb1dc4
root <root@daw2m08.fjeclot.net>
2018-04-23 18:55:49 +0200

    commit1

---
+++ peupagina.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: Peu pàgina per defecte per a noves entrades
+ * Pulgin URL: https://github.com/gelipe1718daw2/pluginwp2_gerliz.git
+ * Description: Crea peus de pàgina per defecte per a noves entrades
+ * Version : 0.1
+ * Author: German Lizondo
+ * Author URL : https://github.com/gelipe1718daw2
+ * License: GNU GLPv3
+ * */
+?>
