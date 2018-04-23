eb981d80622726d2e4f417b9a6b5920b90115cb3
root <root@daw2m08.fjeclot.net>
2018-04-23 19:08:03 +0200

    commit2

--- peupagina.php
+++ peupagina.php
@@ -8,4 +8,13 @@
  * Author URL : https://github.com/gelipe1718daw2
  * License: GNU GLPv3
  * */
+ 
+ function admin_menu_peu_pagina(){
+	 $titolPagina="Peu de pagina per defecte";
+	 $titolEntrada="Peu de pagina per defecte";
+	 $role="administrator";
+	 $id="peu_pagina_per_defecte";
+	 add_menu_page($titolPagina, $titolEntrada,$role,$id);;
+	}
+	 add_action('admin_menu','admin_menu_peu_pagina');
 ?>
